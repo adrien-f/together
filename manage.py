ef1cef74493c61bab67f8314064cb41b66da9d31
Adrien F <adrien.fillon@gmail.com>
2014-06-11 18:47:42 -0400

    Adds the assets manager commands.

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
+from flask.ext.assets import ManageAssets
 from flask.ext.script import Manager
 from flask.ext.migrate import MigrateCommand
 from together.app import create_app
+from together.assets import assets_env
 from together.models import db, migrate
 
 app = create_app()
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command("assets", ManageAssets(assets_env))
 
 if __name__ == '__main__':
     manager.run()
